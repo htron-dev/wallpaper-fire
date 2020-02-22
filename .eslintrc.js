d0f8471b6e69b60126e8424f80b9afb9d071c406
Henrique de Oliveira <henryqueoli@hotmail.com>
2020-02-22 00:58:59 -0300

    Eslint - update eslint to 6.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,54 @@
 module.exports = {
     root: true,
+
     env: {
         node: true
     },
-    "extends": [
+
+    extends: [
         "plugin:vue/essential",
         "@vue/standard",
         "@vue/typescript"
     ],
+
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-        "quotes": ["error", "double"],
-        "indent": ["error", 4],
-        "semi": ["error", "always"],
+        quotes: ["error", "double"],
+        indent: ["error", 4],
+        semi: ["error", "always"],
         "vue/html-indent": ["error", 4]
     },
+
     parserOptions: {
-        parser: "@typescript-eslint/parser"
+        parser: "@typescript-eslint/parser",
+        ecmaVersion: 2020
     },
+
     overrides: [
         {
             files: [
-                "**/__tests__/*.{j,t}s?(x)"
+                "./__tests__/*.{j,t}s?(x)"
+            ],
+            env: {
+                mocha: true
+            }
+        },
+        {
+            files: [
+                "**/__tests__/**/*.spec.{j,t}s?(x)",
+                "**/__tests__/**/*.test.{j,t}s?(x)",
             ],
             env: {
                 mocha: true
             }
         }
+    ],
+
+    extends: [
+        "plugin:vue/essential",
+        "@vue/standard",
+        "@vue/typescript",
+        "@vue/typescript/recommended"
     ]
 };
