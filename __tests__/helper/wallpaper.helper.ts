5dde8909570ecdf4c79b114c2f588133c6ac8efe
Henrique de Oliveira <henryqueoli@hotmail.com>
2020-02-23 00:32:04 -0300

    Tests - use faker with wallpaper helper

--- __tests__/helper/wallpaper.helper.ts
+++ __tests__/helper/wallpaper.helper.ts
@@ -1,13 +1,13 @@
 import { Wallpaper } from "@/store";
 import faker from "faker";
-
-export function createWallpaperList (qtn: number = 5): Wallpaper[] {
-    const source: string = require("../../resources/wallpapers/images/sample-1.jpg");
-    const wallpapers: Wallpaper[] = []
+export function createWallpaperList (qtn = 5): Wallpaper[] {
+    const wallpapers: Wallpaper[] = [];
+    let source;
     for (let i = 0; i < qtn; i++) {
+        source = faker.image.dataUri(400, 400);
         wallpapers.push({
-            id: i + 1,
-            title: `Wallpaper - ${i + 1}`,
+            id: faker.random.uuid(),
+            title: faker.name.title(),
             path: source,
             thumb: source,
             extname: ".jpg",
